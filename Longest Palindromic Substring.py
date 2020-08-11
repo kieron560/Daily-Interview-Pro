7120e102cf50734ff242d9d260dc0321e57ae161
kieron560 <kieron560@gmail.com>
2020-08-12 00:42:37 +0800

    longest palindromic string

---
+++ Longest Palindromic Substring.py
@@ -0,0 +1,27 @@
+# Input: "banana"
+# Output: "anana"
+
+# Input: "million"
+# Output: "illi"
+
+class Solution: 
+    def longestPalindrome(self, s):
+      # Fill this in.
+      result = ""
+      
+      for i in range(len(s)):
+          for j in range(len(s) + 1):
+            current = s[i:j]
+            if current == current[::-1]:
+                if len(current) > len(result):
+                    result = current
+                    
+      return result
+
+        
+# Test program
+# s = "tracecars"
+# s= "aaa"
+s = "million"
+print(str(Solution().longestPalindrome(s)))
+# racecar
